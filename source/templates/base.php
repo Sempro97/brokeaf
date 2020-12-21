105d297e9dd469e9bf9016fb0eff43002b10a33d
Franco Righetti <franco.righetti@studio.unibo.it>
2020-12-21 18:04:45 +0100

    Use PHP CS Fixer rule set

--- source/templates/base.php
+++ source/templates/base.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" href="font-awesome/css/all.min.css" />
-    <title><?php echo $template["title"]; ?></title>
+    <title><?php echo $template['title']; ?></title>
   </head>
   <body>
-<?php require_once "templates/header.php"; ?>
-<?php require_once $template["content"]; ?>
+<?php require_once 'templates/header.php'; ?>
+<?php require_once $template['content']; ?>
   </body>
 </html>
--- source/templates/index.php
+++ source/templates/index.php
@@ -1,4 +1,4 @@
-<?php foreach ($template["items"] as $item): ?>
+<?php foreach ($template['items'] as $item) { ?>
   <div class="container-fluid">
     <div class="row">
       <div class="col-12 p-1">
@@ -6,12 +6,12 @@
           <div class="media">
             <img src="https://via.placeholder.com/150" class="mr-3" alt="" />
             <div class="align-self-center media-body">
-              <h2><?php echo $item["name"]; ?></h2>
-              <span><?php echo $item["price"]; ?>&euro;</span>
+              <h2><?php echo $item['name']; ?></h2>
+              <span><?php echo $item['price']; ?>&euro;</span>
             </div>
           </div>
         </article>
       </div>
     </div>
   </div>
-<?php endforeach; ?>
+<?php } ?>
