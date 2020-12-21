1060777bb75d9aacf5b89877282e03fc3dd7eacc
Franco Righetti <franco.righetti@studio.unibo.it>
2020-12-21 16:54:54 +0100

    Use database in index

--- source/index.php
+++ source/index.php
@@ -1,7 +1,8 @@
 <?php
-  // require 'utilities/database.php';
-  // $database = new Database();
-  $template["title"] = "Index";
-  $template["content"] = "templates/index.php";
-  // $template["items"] = $database->get_random_items(5);
-  require 'templates/base.php';
+
+require 'utilities/database.php';
+$database = new Database();
+$template["title"] = "Index";
+$template["content"] = "templates/index.php";
+$template["items"] = $database->get_random_items(5);
+require 'templates/base.php';
