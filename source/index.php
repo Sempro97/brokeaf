ce6cbffbb09a53d8b782d8357426a5946922d361
Mattia Semprini <mattia.semprini2@studio.unibo.it>
2020-12-21 11:59:36 +0100

    'Merge' example code from 'php-utilities' branch

--- source/index.php
+++ source/index.php
@@ -1,14 +1,19 @@
 <?php
-
-$serverName = "mysql";
-$username = "root";
-$password = "root";
-$dbname = "brokeaf";
-
-$connection = new mysqli($serverName, $username, $password, $dbname);
+session_start();
+require_once("utilities/database.php");
+$db = new Database();
 
 // Check connection
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-}
-echo "Connected successfully";
+$items = $db->get_random_items(5);
+$insert_items = array();
+foreach($items as $item){
+  echo "<h2>" . $item["name"] . "</h2>";
+  array_push($insert_items, $item["name"]);
+  echo "<p> Insert " . $item["name"] . " in array of inserted </p>";
+  }
+?>
+
+<html>
+<body>
+</body>
+</html>
--- source/utilities/database.php
+++ source/utilities/database.php
@@ -57,3 +57,4 @@ class Database
         return true;
     }
 }
+?>
