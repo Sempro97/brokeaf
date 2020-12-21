2d7aec737be4fcfbf399cec26f5836229da8ea88
Franco Righetti <franco.righetti@studio.unibo.it>
2020-12-21 18:27:43 +0100

    Add alternative attribute to item images

--- source/templates/index.php
+++ source/templates/index.php
@@ -4,7 +4,7 @@
       <div class="col-12 pb-2">
         <article class="bg-light border p-1">
           <div class="media">
-            <img src="https://via.placeholder.com/150" class="mr-3" alt="" />
+            <img src="https://via.placeholder.com/150" class="mr-3" alt="<?php echo $item['name']; ?>" />
             <div class="align-self-center media-body">
               <h2><?php echo $item['name']; ?></h2>
               <span><?php echo $item['price']; ?>&euro;</span>
