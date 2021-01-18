56167e5b794ed4758662d1782732e240c331ff53
Franco Righetti <franco.righetti@studio.unibo.it>
2021-01-18 19:12:09 +0100

    Include JavaScript dependencies in header

--- source/templates/base.php
+++ source/templates/base.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" href="font-awesome/css/all.min.css" />
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
     <title><?php echo $template['title']; ?></title>
   </head>
   <body>
