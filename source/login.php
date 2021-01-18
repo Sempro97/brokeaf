855841f6beb95bda6fd2e9d1ac22b1486534ef18
Mirco Casadei <mirco.casadei3@studio.unibo.it>
2021-01-18 18:43:30 +0100

    Add redirect button to register page

--- source/login.php
+++ source/login.php
@@ -48,7 +48,7 @@
       </div>
     </form>
       <div class="form-group">
-        <button type="submit" class="btn btn-primary btn-block">Create account</button>
+        <button type="submit" class="btn btn-primary btn-block" onclick="window.location.href='register.php'">Create account</button>
       </div>
     </div>
   </body>
