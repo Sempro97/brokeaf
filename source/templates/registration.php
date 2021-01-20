a4ff5daaf90a9411026380c1f7e227e023069387
Semprini Mattia <--list>
2021-01-20 21:29:02 +0100

    Remove unused import

--- source/templates/registration.php
+++ source/templates/registration.php
@@ -9,8 +9,6 @@
     <script src="js/jquery.mobilePhoneNumber.js" type="text/javascript"></script>
     <script src="js/sha512.js" type="text/javascript"></script>
     <script src="js/utils.js" type="text/javascript"></script>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="font-awesome/css/all.min.css" />
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
