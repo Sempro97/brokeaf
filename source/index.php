e313ffa27c1df08d0971be954bfb32120d589f93
Franco Righetti <franco.righetti@studio.unibo.it>
2021-01-21 22:17:19 +0100

    Add dynamic script import

--- source/index.php
+++ source/index.php
@@ -3,6 +3,10 @@
 require_once 'utilities/database.php';
 $database = new Database();
 $template['title'] = 'Index';
+$template['scripts'] = [
+    'js/jquery-3.4.1.min.js',
+    'bootstrap/js/bootstrap.bundle.min.js',
+];
 $template['content'] = 'templates/index.php';
 $template['items'] = $database->get_random_items(5);
 require_once 'templates/base.php';
