62822d3fb4fe78c11d1b5e7c0922f2c9e269909d
Semprini Mattia <--list>
2021-01-22 11:13:05 +0100

    Add GET request i.e. http://localhost:8080/product.php?id=4943693566

--- source/product.php
+++ source/product.php
@@ -5,8 +5,13 @@ $database = new Database();
 $database->sec_session_start();
 $template['title'] = 'Articolo not found!';
 $template['content'] = 'templates/product.php';
-//Hardcoded
-$template['item'] = $database->get_item(1952322448);
+
+$product = -1;
+if(isset($_GET["id"])){
+    $product = $_GET["id"];
+}
+
+$template['item'] = $database->get_item($product);
 $template['imagePath'] = $database->get_img_item($template['item'][0]['serialCode'])[0]['path'];
 
 require_once 'templates/base.php';
