1db35e44018034507f384467385148f54e2a0817
Semprini Mattia <--list>
2021-01-23 19:33:16 +0100

    Initial

--- source/index.php
+++ source/index.php
@@ -2,6 +2,7 @@
 
 const ITEM_COUNT = 5;
 require_once 'utilities/database.php';
+require_once 'utilities/cookies.php';
 $database = new Database();
 $template['title'] = 'Index';
 $template['scripts'] = [
@@ -12,4 +13,11 @@ $template['content'] = 'templates/index.php';
 $search = $_GET['search'];
 $template['search'] = $search;
 $template['items'] = $search ? $database->get_items_by_name($search, ITEM_COUNT) : $database->get_random_items(ITEM_COUNT);
+/* sec_session_start();
+if(login("zstiedemann@example.com", 1)){
+    echo "Login eseguito!";
+} else {
+    echo "login errore!";
+} */
+
 require_once 'templates/base.php';
