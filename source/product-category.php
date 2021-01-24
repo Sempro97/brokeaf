b2ba3a3353ad0599bcf0980cd1cb3f31d75991de
Semprini Mattia <--list>
2021-01-24 16:43:55 +0100

    Add untracked file

--- source/product-category.php
+++ source/product-category.php
@@ -1,1 +1,2 @@
-<!-- Il contenuto e' lo stesso di index ma mostra una lista con gli item di una sola categoria -->
+<!-- Il contenuto e' lo stesso di index ma mostra una lista con gli item di una sola categoria
+si detrmite dal parametro id in get la categoria -->
