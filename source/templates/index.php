c9e7b6b67655fe79280fbc110ca76f53b68d1462
Franco Righetti <franco.righetti@studio.unibo.it>
2021-01-25 20:14:24 +0100

    Revert index changes

--- source/templates/index.php
+++ source/templates/index.php
@@ -4,7 +4,7 @@
         <div class="col">
           <article class="bg-light border p-1">
             <div class="media">
-              <img src="img\<?php echo $img[0]['path']; ?>.jpg" class="mr-3" alt="<?php echo $item['name']; ?>" />
+              <img src="https://via.placeholder.com/150" class="mr-3" alt="<?php echo $item['name']; ?>" />
               <div class="align-self-center media-body">
                 <h2><?php echo $item['name']; ?></h2>
                 <span><?php echo $item['price']; ?>&euro;</span>
