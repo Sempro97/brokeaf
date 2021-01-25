cfd008835ad98fa2e084c5bc4828def4b4fb20c6
Semprini Mattia <--list>
2021-01-25 18:01:55 +0100

    Adjust image

--- source/templates/index.php
+++ source/templates/index.php
@@ -3,11 +3,14 @@
   $image = $database->get_img_item($item['serialCode']);?>
       <div class="row pb-2">
         <div class="col">
-          <article class="bg-light border p-1">
+          <article class="bg-light border p-1 ">
             <div class="media">
-              <img src="../images/<?php echo $image[0]['path']; ?>" class="mr-3" alt="<?php echo $item['name']; ?>" />
-              <div class="align-self-center media-body">
-                <h2><?php echo $item['name']; ?></h2>
+            <img class="w-25 align-self-center mr-3 img-thumbnail"
+            src="../images/<?php echo $image[0]['path']; ?>" 
+             alt="<?php echo $item['name']; ?>"
+             style="width:60px" />
+              <div class="align-self-center media-body ">
+                <h2 class="h4"><?php echo $item['name']; ?></h2>
                 <span><?php echo $item['price']; ?>&euro;</span>
               </div>
             </div>
