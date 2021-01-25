ffc2aa5671912ccc4742178eb9970469ca847adc
Semprini Mattia <--list>
2021-01-25 23:21:45 +0100

    Update folder ref

--- source/api/add-item.php
+++ source/api/add-item.php
@@ -25,7 +25,7 @@ if ($item) {
 
 // Constants
 const ALLOWED_IMAGE_EXTENSIONS = ['jpeg', 'jpg', 'png'];
-const IMAGES_FOLDER = '/var/www/html/images/';
+const IMAGES_FOLDER = '../images/';
 const MAXIMUM_ALLOWED_IMAGE_SIZE = 2000000;
 // Check for errors.
 $error = $_FILES['image']['error'];
