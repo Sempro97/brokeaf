109c2f47268e55093e414ea98c1c8c4786e138c0
Franco Righetti <franco.righetti@studio.unibo.it>
2021-01-25 20:11:19 +0100

    Remove registration

--- source/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'utilities/database.php';
-$database = new Database();
-$database->sec_session_start();
-$template['title'] = 'Registration';
-$template['content'] = 'templates/registration.php';
-$template['items'] = $database->get_random_items(5);
-require_once 'templates/base.php';
