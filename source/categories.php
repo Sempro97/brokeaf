1197344ba19f40fdeb13777573677d20f04cfd8c
Franco Righetti <franco.righetti@studio.unibo.it>
2021-01-25 17:42:04 +0100

    Rename files

--- source/categories.php
+++ source/categories.php
@@ -1,12 +1,12 @@
 <?php
 
 require_once 'utilities/bootstrap.php';
-$template['title'] = 'List of categories';
+$template['title'] = 'Categories';
 $template['scripts'] = [
     'js/jquery-3.4.1.min.js',
     'bootstrap/js/bootstrap.bundle.min.js',
 ];
-$template['header'] = 'List of categories';
-$template['content'] = 'templates/category.php';
+$template['header'] = 'Categories';
+$template['content'] = 'templates/categories.php';
 $template['categories'] = $database->get_categories();
 require_once 'templates/base.php';
