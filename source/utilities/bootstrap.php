a3ddbe0882094e0284614377b268b9fb517b7799
Franco Righetti <franco.righetti@studio.unibo.it>
2021-01-25 19:42:01 +0100

    Move secure session start

--- source/utilities/bootstrap.php
+++ source/utilities/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
-session_start();
+require_once '/var/www/html/utilities/secure_session_start.php';
+secure_session_start();
 require_once '/var/www/html/utilities/database.php';
 $database = new Database();
 // $database->login('adrain.johnson@example.com', 89); // User.
