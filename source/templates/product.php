da9a7fd17eac3cb948ef004030c94a80a8f6b47b
Franco Righetti <franco.righetti@studio.unibo.it>
2021-01-27 23:17:24 +0100

    Refactor product page

--- source/templates/product.php
+++ source/templates/product.php
@@ -1,87 +1,80 @@
-<!-- Page Content  
-isVerificated`, `description`, `price`, `name`, `serialCode`, `email`, `Kin_name`-->
+<!-- Page Content: isVerificated`, `description`, `price`, `name`, `serialCode`, `email`, `Kin_name`-->
 <div class="container">
   <div class="row">
-    <div class="col-sm-12 rounded pt-2">
-      <div class="card text-center mt-4">
-        <h3
-          class="card-header text-center"
-        ><?php echo $template['item']['name']; ?></h3>
-        <div style="max-width: 50%; height: 50%;">
-          <img
-            class="d-block img-thumbnail Image-fluid"
-            src="../images/<?php echo $image[0]['path']; ?>"
-            alt="image of <?php  echo $template['item']['name'];
-          ?>"
-            style="max-width: 75%; height: 50%;"
-          />
+    <div class="col rounded">
+      <div class="card">
+        <div class="card-header text-center">
+          <span class="h3"><?php echo $template['item']['name']; ?></span>
         </div>
-        <div class="text-left">
-          <h4>
-            Price:
-            <small><?php echo $template['item']['price']; ?></small>
-          </h4>
-          <p>
-            Status:
-            <small>Available</small>
-          </p><?php if($database->is_user($email) ){ ?>
-          <form id="shoppingButton">
-            <label for="quantity-item">Quantity:</label>
-            <div class="form-row">
-              <div class="col">
-                <div
-                  class="btn-toolbar mb-3"
-                  role="toolbar"
-                  aria-label="Toolbar with button groups"
-                >
-                  <div
-                    class="btn-group ml-2 mr-2"
-                    role="group"
-                    aria-label="First group"
-                  >
-                    <button id="minus-item" class="btn input-group btn-dark">-</button>
+        <div class="card-body">
+          <div class="container">
+            <div class="row pb-3">
+              <div class="col p-0 mx-auto text-center">
+                <img class="img-fluid img-thumbnail" style="max-height: 400px;" src="../images/<?php echo $image[0]['path']; ?>" alt="Image of <?php  echo $template['item']['name']; ?>" />
+              </div>
+            </div>
+            <!-- Price -->
+            <div class="row">
+              <div class="col p-0">
+                <span class="h4">Price: <?php echo $template['item']['price']; ?></span>
+              </div>
+            </div>
+            <!-- Status -->
+            <div class="row pb-3">
+              <div class="col p-0">
+                <span class="h6">Status: Available</span>
+              </div>
+            </div>
+            <!-- Quantity -->
+<?php if($database->is_user($email) ){ ?>
+            <div class="row">
+              <div class="col p-0">
+                <form id="shoppingButton">
+                  <label for="quantity-item">Quantity:</label>
+                  <div class="form-row mb-2">
+                    <div class="col">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <button id="minus-item" class="btn btn-dark"><span class="fas fa-minus"></span></button>
+                        </div>
+                          <input type="text" id="quantity-item" value="0" class="input-group-text" disabled />
+                        <div class="input-group-append">
+                          <button id="plus-item" class="btn btn-dark"><span class="fas fa-plus"></span></button>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <input
-                    type="text"
-                    style="width: 30%;"
-                    id="quantity-item"
-                    value="0"
-                    class="input-group-text"
-                    disabled
-                  />
-                  <div
-                    class="btn-group ml-2"
-                    role="group"
-                    aria-label="First group"
-                  >
-                    <button
-                      id="plus-item"
-                      class="btn input-group-append btn-dark"
-                    >+</button>
+                  <div class="form-row mb-1">
+                    <div class="col">
+                      <button id="add-to-cart" type="button" class="btn btn-success">
+                        <span class="fas fa-cart-arrow-down"></span>
+                      </button>
+                    </div>
                   </div>
+                </form>
+              </div>
+            </div>
+<?php }?>
+            <!-- Sold by -->
+              <div class="row pt-3">
+                <div class="col p-0">
+                  <span class="h6">Sold by</span>
+                  <a href="https://www.w3schools.com"><?php echo $template['item']['emailSeller']; ?></a>
                 </div>
-                <div class="form-row ml-2 mt-2">
-                  <button
-                    id="add-to-cart"
-                    type="button"
-                    class="btn btn-success"
-                  >
-                    <span class="fas fa-cart-arrow-down"></span>
-                  </button>
+              </div>
+              <div class="row pt-3">
+                <div class="col p-0">
+                  <div class="alert alert-danger d-none invisible" role="alert"></div>
+                </div>
+              </div>
+              <!-- Description -->
+              <div class="row">
+                <div class="col p-0">
+                  <span class="h6"><?php echo $template['item']['description']; ?></span>
                 </div>
               </div>
             </div>
-          </form><?php }?>
-          <div class="pt-2">
-            <p>
-              Sold by
-              <a
-                href="https://www.w3schools.com"
-              ><?php echo $template['item']['emailSeller']; ?></a>
-            </p>
           </div>
-          <div class="alert alert-danger d-none invisible" role="alert"></div>
-          <p class="card-text"><?php echo $template['item']['description']; ?></p>
         </div>
       </div>
     </div>
